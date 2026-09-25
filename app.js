c8fed4ea8fa547ab1565fbb99eb23524ea68af2f
JavierBDG <javierbarrosodiaz2006@gmail.com>
2026-09-25 18:30:01 +0200

    Añadir modo noche

--- app.js
+++ app.js
@@ -12,3 +12,16 @@ boton.addEventListener("click", function () {
     contador++;
     aviso.textContent = "Has pulsado el botón " + contador + " veces.";
 });
+// Recuperamos el botón del modo noche
+const botonModo = document.getElementById("modonoche");
+
+// Cambiamos entre modo oscuro y modo claro
+botonModo.addEventListener("click", function () {
+    document.body.classList.toggle("tema-claro");
+
+    if (document.body.classList.contains("tema-claro")) {
+        botonModo.textContent = "Modo oscuro";
+    } else {
+        botonModo.textContent = "Modo claro";
+    }
+});
